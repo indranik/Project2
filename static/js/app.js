ccf5a1577837e2b411748a796c88a92f160ff4a0
ninja85 <niyatihd@gmail.com>
2018-06-08 09:19:52 -0700

    Added basic code in each file to test sync between files. Works great

---
+++ static/js/app.js
@@ -0,0 +1,36 @@
+//***************************************/
+//*************TEST BLOCK****************/
+//***************************************/
+
+//  Calling the renderHTMLtest function to test the sync amongst .py .html and .css
+renderHTMLtest();
+
+function renderHTMLtest() {
+  // Getting a reference to the element on the page with an ID of app
+  var app = document.querySelector("#test");
+
+  // Storing a string into a variable
+  var headingMessage = "Prelimnary TEST to check the sync between diff files";
+
+  // Storing a string into the paragraphMessage variable.
+  var paragraphMessage = "Looks like it works!!!! ";
+  paragraphMessage += "AWESOME!!!! ";
+  paragraphMessage += "Lets get started!!!.";
+
+
+  // Create a heading element w/ document.createElement
+  var heading = document.createElement("h1");
+  var paragraph = document.createElement("p");
+
+  // Update the innerHTML property of these elements
+  heading.innerHTML = headingMessage;
+  paragraph.innerHTML = paragraphMessage;
+
+  // Use 'appendChild' to put the heading and paragraph into the `app` container
+  app.appendChild(heading);
+  app.appendChild(paragraph);
+}
+
+//***************************************/
+//***********END TEST BLOCK**************/
+//***************************************/
