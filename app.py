eaa4fdbfebe4a852de5eb78bd18ac1281bbdb9e7
ninja85 <niyatihd@gmail.com>
2018-06-11 11:01:59 -0700

    Added parsed data into dictionary and then to app.py and fetching it into app.js

--- app.py
+++ app.py
@@ -18,6 +18,41 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+#@app.route("/selectlist")
+#def get_select_list():
+#    data = pd.read_csv("resources/data/summaryData.csv")
+#    data = data.sort_values('TSA')
+#    data_selectlist = data[['TSA','Dis_SubDis', 'LUCategory']]
+#    data_selectlist.reset_index(inplace=True, drop=True)
+#    
+#    initial_val = "Herndon TSA"
+#    district_list = []
+#    landuse_list = []
+#    final_list = {}
+#    final_list['district'] = []
+#    final_list['landuse'] = []
+#
+#    for row1 in range(len(data_selectlist.TSA)-1):
+#        if data_selectlist.TSA[row1] == initial_val:
+#            if data_selectlist.Dis_SubDis[row1] not in district_list:
+#                district_list.append(data_selectlist.Dis_SubDis[row1])
+#            if data_selectlist.LUCategory[row1] not in landuse_list:
+#                landuse_list.append(data_selectlist.LUCategory[row1])
+#        
+#        else:
+#            final_list['district'].append(district_list)
+#            final_list['landuse'].append(landuse_list)        
+#            initial_val = data_selectlist.TSA[row1]
+#            district_list = []
+#            landuse_list = []
+#    final_list['district'].append(district_list) 
+#    final_list['landuse'].append(landuse_list)
+#
+#    select_dropdown_list = [{"Herndon TSA":{"district":final_list['district'][0],"landuse":final_list['landuse'][0]},
+#                "Reston Town Center TSA":{"district":final_list['district'][1],"landuse":final_list['landuse'][1]},
+#                "Wiehle-Reston East TSA":{"district":final_list['district'][2],"landuse":final_list['landuse'][2]}}]
+#        
+#    return jsonify(select_dropdown_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -38,8 +38,8 @@
 //***************************************/
 //*********CREATE SELECT LIST************/
 //***************************************/
-d3.csv('../resources/data/summaryData.csv', (error, response) => {
-  if (error) return console.warn(error);
+d3.csv('/resources/data/summaryData.csv', (error, response) => {
+  // if (error) return cons ole.warn(error);
   var todoList = document.querySelector("#dropdownlist1");
   todoHTML='';
   var duplicate = [];
@@ -51,6 +51,40 @@ d3.csv('../resources/data/summaryData.csv', (error, response) => {
       duplicate.push(response[i].TSA);
   } 
 });
+
+// var temp = [{'Herndon TSA': {'district': ['Herndon Station', 'Great Oak', 'Woodland Park'], 
+//                              'landuse': ['Residential Mixed Use', 'Residential', 'Mixed Use', 'Transit Station Mixed Use']},
+//              'Reston Town Center TSA': 
+//                             {'district': ['Reston Town Center Station North', 'Town Center Urban Core', 
+//                                           'Central Sunrise Valley', 'West Fountain Drive', 'Town Center West', 
+//                                           'Reston Heights', 'Old Reston Avenue', 'East Fountain Drive', 
+//                                           'Reston Town Center Station South'], 
+//                              'landuse': ['Residential Mixed Use', 'Town Center Urban Core Mixed Use', 'Office', 'Mixed Use', 
+//                                          'Industrial', 'Transit Station Mixed Use', 'Town Center North Mixed Use']}, 
+//              'Wiehle-Reston East TSA': 
+//                             {'district': ['Wiehle Station North', 'Sunset Hills', 'Reston East', 'Wiehle Station South'], 
+//                              'landuse': ['Residential Mixed Use', 'Transit Station Mixed Use', 'Mixed Use', 'Government / Institutional']}}];
+
+// d3.json('/selectlist', (error, response) => {
+//   if (error) return console.warn(error);
+
+//   var todoList = document.querySelector("#test");
+//   var todoHTML='';
+//   var sample = 'Herndon TSA'
+//   var j = 0;
+
+//   for (i = 0; i < response.length; i++) {
+//       var object = response[j];
+//       // otu_ids_list = object[sample]['otu_ids'];
+//       if (Object.keys(object) == sample) {
+//           // todoHTML = '';
+//           todoHTML += "<p> AGE: " + object['Herndon TSA']['district'][0] + "</p>";
+//           todoList.innerHTML = todoHTML;
+//       }
+//       j++;
+//   }
+
+// })
 //***************************************/
 //***********END SELECT LIST*************/
 //***************************************/
