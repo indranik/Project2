fd1abcfe940ef057c4da71334e044795ddd1e131
Guirlyn Olivar <guirlyn@gmail.com>
2018-06-12 17:11:32 -0700

    add gauge for initial general view

--- static/js/app.js
+++ static/js/app.js
@@ -169,10 +169,92 @@ d3.json('/selectlist', (error, response) => {
 //***********END SELECT LIST*************/
 //***************************************/
 
-d3.json('/submit', (error, response) =>{
+/*
+
+Gauge
+
+*/
+
+function gauge(scenario, category, value){
+  // Trig to calc meter point
+  var degrees = 180 - (value*1.8);
+  var radius = .5;
+  var radians = degrees * Math.PI / 180;
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  // Path: may have to change to create a better triangle
+  var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+      pathX = String(x),
+      space = ' ',
+      pathY = String(y),
+      pathEnd = ' Z';
+  var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+  var data = [{ type: 'scatter',
+  x: [0], y:[0],
+      marker: {size: 28, color:'850000'},
+      showlegend: false,
+      name: 'value',
+      text: value,
+      hoverinfo: 'text+name'},
+  { values: [50/9,50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9,50/9,50],
+  rotation: 90,
+  text: ['100', '', '', '', '', '',
+              '', '', '0',''],
+  textinfo: 'text',
+  textposition:'inside',
+  marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                          'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                          'rgba(205, 202, 42, .5)', 'rgba(210, 209, 95, .5)',
+                          'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                          'rgba(240, 216, 145, .5)',
+                          'rgba(255, 255, 255, 0)']},
+  //labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4',
+  //'2-3', '1-2', '0-1',''],
+  hoverinfo: 'text',
+  hole: .5,
+  type: 'pie',
+  showlegend: false
+  }];
+
+  var layout = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: '850000',
+      line: {
+          color: '850000'
+      }
+      }],
+  title: 'Scenario: ' + scenario,
+  xaxis: {zeroline:false, showticklabels:false, title: category,
+              showgrid: false, range: [-1, 1]}, 
+  yaxis: {zeroline:false, showticklabels:false,
+              showgrid: false, range: [-1, 1]}
+  };
+
+  Plotly.newPlot('gauge'+scenario+category, data, layout);
+}
+
+
+
+d3.json('/submit', (error, response) => {
   if (error) return console.warn(error);
   
+  // draw gauge based on example table loaded in response
   var table = document.querySelector("#table");
+  
+  gauge('Existing', "Residential", 27);
+  gauge('Plan', "Residential",52);
+  gauge('Approved', "Residential",32);
+  gauge('Review', "Residential",43);
+  
+  gauge('Existing', "NonResidential", 73);
+  gauge('Plan', "NonResidential",48);
+  gauge('Approved', "NonResidential",68);
+  gauge('Review', "NonResidential",57);
+  
 
   table.innerHTML = response;
 
